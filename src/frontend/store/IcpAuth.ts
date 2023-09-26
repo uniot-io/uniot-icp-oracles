fc5e49ace660963b7564f051e435e243544a3ae8
vldmkr <vladmakaryan@gmail.com>
2023-09-26 07:08:22 +0300

    upgrade icpAuthStore

--- src/frontend/store/IcpAuth.ts
+++ src/frontend/store/IcpAuth.ts
@@ -1,42 +1,66 @@
 import { defineStore } from 'pinia'
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { AuthClient } from '@dfinity/auth-client'
 import { HttpAgent, Identity } from '@dfinity/agent'
 import router from '@/router'
 
+// Refer to documentation: https://agent-js.icp.xyz/
 export const useIcpAuthStore = defineStore('icpAuthStore', () => {
+  let authClient: AuthClient | undefined // can't be stored in ref...
   const authenticated = ref(false)
-  let authClient: AuthClient | undefined = undefined // can't be stored in ref...
   const identity = ref<Identity>()
   const agent = ref<HttpAgent>()
   const principal = ref('')
   const isAuthenticated = computed(() => authenticated.value)
 
-  async function login() {
-    // create an auth client
-    authClient = await AuthClient.create()
-    // start the login process and wait for it to finish
-    authenticated.value = await new Promise((resolve, reject) => {
-      authClient?.login({
-        identityProvider: import.meta.env.VITE_APP_II_URL,
-        onSuccess: () => {
-          resolve(true)
-        },
-        onError: (error) => {
-          reject(error)
-        },
-        derivationOrigin: import.meta.env.VITE_APP_II_DERIVATION
-      })
-    })
+  watch(authenticated, async (success) => {
+    if (success) {
+      identity.value = authClient?.getIdentity()
+      principal.value = (await identity.value!.getPrincipal()).toString()
+      agent.value = new HttpAgent({ identity: identity.value })
+    }
+  })
 
-    // At this point we're authenticated, and we can get the identity from the auth client:
-    identity.value = authClient.getIdentity()
-    // Using the identity obtained from the auth client, we can create an agent to interact with the IC.
-    agent.value = new HttpAgent({ identity: identity.value })
-    principal.value = (await agent.value!.getPrincipal()).toString()
+  async function init(): Promise<void> {
+    if (authClient === undefined) {
+      authClient = await AuthClient.create({
+        keyType: 'Ed25519',
+        idleOptions: {
+          // call logout & reload window if idle for <idleTimeout> milliseconds
+          idleTimeout: 60 * 10 * 1000 // 10 minutes
+        }
+      })
+    }
+    authenticated.value = await authClient.isAuthenticated()
   }
 
-  async function logout() {
+  async function login(): Promise<void> {
+    await init()
+    if (authenticated.value) {
+      return
+    }
+
+    authenticated.value = await new Promise((resolve, reject) => {
+      authClient
+        ?.login({
+          identityProvider: import.meta.env.VITE_APP_II_URL,
+          maxTimeToLive: BigInt(1 * 24 * 60 * 60 * 1000 * 1000 * 1000), // 1 day in nanoseconds
+          onSuccess: () => {
+            resolve(true)
+          },
+          onError: (error) => {
+            reject(error)
+          },
+          derivationOrigin: import.meta.env.VITE_APP_II_DERIVATION
+        })
+        .catch((error) => {
+          console.error(error)
+          throw error
+        })
+    })
+  }
+
+  async function logout(): Promise<void> {
     try {
       await authClient?.logout()
     } catch (error) {
@@ -50,6 +74,11 @@ export const useIcpAuthStore = defineStore('icpAuthStore', () => {
       await router.push('login')
     }
   }
+
+  init().catch((error) => {
+    console.error(error)
+    throw error
+  })
 
   return { isAuthenticated, login, logout, principal }
 })
